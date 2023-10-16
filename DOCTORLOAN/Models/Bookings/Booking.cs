b456bdc7c9e21fe4aa75e051dfffeefd92911b97
coder2 <coder2.dl@doctorloan.vn>
2023-10-16 08:09:25 +0700

    Call api BÃooking

--- DOCTORLOAN/Models/Bookings/Booking.cs
+++ DOCTORLOAN/Models/Bookings/Booking.cs
@@ -2,17 +2,17 @@
 
 public class Booking
 {
-    //public BookingType Type { get; set; }
-    public int CustomerId { get; set; }
-    public int? CustomerAddressId { get; set; }
+    public int Type { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; } 
+    public string  Phone { get; set; }
     public int BookingTimes { get; set; }
     public DateOnly BookingDate { get; set; }
     public TimeOnly BookingStartTime { get; set; }
     public TimeOnly BookingEndTime { get; set; }
-    //public BookingStatus Status { get; set; }
+    public string AddressLine { get; set; }
+    public int ProvinceId { get; set; }
+    public int DistrictId { get; set; }
+    public int WardId { get; set; }
     public string Noted { get; set; }
-
-    //public virtual Customer Customer { get; set; }
-    //public virtual CustomerAddress CustomerAddresses { get; set; }
-
 }
