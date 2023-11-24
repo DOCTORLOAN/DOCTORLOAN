a69b55482ae27c5df9762a8ab8402cc047b9da3f
coder2 <coder2.dl@doctorloan.vn>
2023-11-24 13:44:14 +0700

    function add to cáºart 01

--- DOCTORLOAN/Controllers/CartController.cs
+++ DOCTORLOAN/Controllers/CartController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DOCTORLOAN.Models.Contents;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DOCTORLOAN.Controllers
 {
@@ -7,6 +8,40 @@ namespace DOCTORLOAN.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> AddToCart(int id, int SoLuong, string type = "Normal")
+        {
+            var myCart = Carts;
+            var item = myCart.SingleOrDefault(p => p.MaHh == id);
+
+            if (item == null)//chưa có
+            {
+                var hangHoa = _context.HangHoa.SingleOrDefault(p => p.MaHh == id);
+                item = new CartItem
+                {
+                    MaHh = id,
+                    TenHH = hangHoa.TenHh,
+                    DonGia = hangHoa.DonGia.Value,
+                    SoLuong = SoLuong,
+                    Hinh = hangHoa.Hinh
+                };
+                myCart.Add(item);
+            }
+            else
+            {
+                item.SoLuong += SoLuong;
+            }
+            HttpContext.Session.Set("GioHang", myCart);
+
+            if (type == "ajax")
+            {
+                return Json(new
+                {
+                    SoLuong = Carts.Sum(c => c.SoLuong)
+                });
+            }
+            return RedirectToAction("Index");
         }
 
         public IActionResult Payment()
